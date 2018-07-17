6d8320f0a295ef96c6fdb6465b81b6a5e755cc50
1224832958@qq.com <1224832958@qq.com>
2018-07-17 20:11:44 +0800

    更新

---
+++ InkePlayer/Class/Me/SXTLoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  SXTLoginViewController.m
+//  InkePlayer
+//
+//  Created by 徐超 on 2018/7/17.
+//  Copyright © 2018年 sd. All rights reserved.
+//
+
+#import "SXTLoginViewController.h"
+
+@interface SXTLoginViewController ()
+
+@end
+
+@implementation SXTLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
