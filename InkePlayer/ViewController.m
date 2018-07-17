4785f3c615fcf3a235c3e471e36f1f28e44c3d4e
1224832958@qq.com <1224832958@qq.com>
2018-07-17 14:47:26 +0800

    Initial Commit

---
+++ InkePlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InkePlayer
+//
+//  Created by 徐超 on 2018/7/17.
+//  Copyright © 2018年 sd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ InkePlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InkePlayer
+//
+//  Created by 徐超 on 2018/7/17.
+//  Copyright © 2018年 sd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
